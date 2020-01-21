721294668cce322128b27bcfe7d8ffb3520dd816
sainiprathmesh <sainiprathmesh@gmail.com>
2020-01-21 18:47:02 +0530

    method created : findTerm, to find nth term of given series.

--- src/Solve_The_Series/Solution.java
+++ src/Solve_The_Series/Solution.java
@@ -1,4 +1,15 @@
 package Solve_The_Series;
 
 public class Solution {
+    static int findTerm(int n) {
+        if (n == 1) {
+            return n;
+        } else {
+            int a = 7;
+            for (int i = 2; i <= n; i++) {
+                a = a * 2 + (i - 1);
+            }
+            return a;
+        }
+    }
 }
