258db8b94f44ef63f8c991c6c09ad83ba305c496
sainiprathmesh <sainiprathmesh@gmail.com>
2020-01-21 19:38:49 +0530

    all method is modified.

--- src/Pendulum_Arrangment/Solution.java
+++ src/Pendulum_Arrangment/Solution.java
@@ -4,35 +4,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-    static void Arrangement(int[] a) {
-        Arrays.sort(a);
-        int op[] = new int[a.length];
-        int mid = (a.length - 1) / 2;
+    static void Arrangement(int[] arr, int n) {
+        Arrays.sort(arr);
+        int[] op = new int[n];
+        int mid = (n - 1) / 2;
         int j = 1, i = 1;
-        op[mid] = a[0];
+        op[mid] = arr[0];
         for (i = 1; i <= mid; i++) {
-            op[mid + i] = a[j++];
-            op[mid - i] = a[j++];
+            op[mid + i] = arr[j++];
+            op[mid - i] = arr[j++];
         }
-        if (a.length % 2 == 0)
-            op[mid + i] = a[j];
-
-        System.out.println("Pendulum arrangement:");
-        for (i = 0; i < a.length; i++)
+        if (n % 2 == 0)
+            op[mid + i] = arr[j];
+        for (i = 0; i < n; i++)
             System.out.print(op[i] + " ");
         System.out.println();
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int testCases = scanner.nextInt();
-        for (int i = 0; i < testCases; i++) {
+        int test = scanner.nextInt();
+        for (int i = 0; i < test; i++) {
             int len = scanner.nextInt();
             int[] arr = new int[len];
             for (int i1 = 0; i1 < len; i1++) {
-                arr[i] = scanner.nextInt();
+                arr[i1] = scanner.nextInt();
             }
-            Arrangement(arr);
+            Arrangement(arr, arr.length);
         }
     }
 }
