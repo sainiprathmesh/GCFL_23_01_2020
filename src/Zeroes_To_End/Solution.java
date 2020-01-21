f9ff123c09744daaee559a3cad5a2a1c09857527
sainiprathmesh <sainiprathmesh@gmail.com>
2020-01-21 18:57:35 +0530

    method completed : Arrangement.

--- src/Zeroes_To_End/Solution.java
+++ src/Zeroes_To_End/Solution.java
@@ -2,6 +2,12 @@ package Zeroes_To_End;
 
 public class Solution {
     static void Arrangement(int[] a) {
-
+        int[] n = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != 0) {
+                n[i] = a[i];
+            }
+            System.out.print(n[i] + " ");
+        }
     }
 }
