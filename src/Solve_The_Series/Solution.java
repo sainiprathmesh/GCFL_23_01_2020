d780ea04aedfd65aa330b1e28581363932a235cb
sainiprathmesh <sainiprathmesh@gmail.com>
2020-01-21 18:49:03 +0530

    method calling and output printing line is added.

--- src/Solve_The_Series/Solution.java
+++ src/Solve_The_Series/Solution.java
@@ -18,5 +18,6 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        System.out.println(findTerm(n));
     }
 }
