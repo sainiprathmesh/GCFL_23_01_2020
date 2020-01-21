0ddb52f575025c576752b9a3b3ee01116c57c486
sainiprathmesh <sainiprathmesh@gmail.com>
2020-01-21 19:21:34 +0530

    Method Completed : Arrangement for Pendulum.

--- src/Pendulum_Arrangment/Solution.java
+++ src/Pendulum_Arrangment/Solution.java
@@ -1,7 +1,24 @@
 package Pendulum_Arrangment;
 
+import java.util.Arrays;
+
 public class Solution {
     static void Arrangement(int[] a) {
+        Arrays.sort(a);
+        int op[] = new int[a.length];
+        int mid = (a.length - 1) / 2;
+        int j = 1, i = 1;
+        op[mid] = a[0];
+        for (i = 1; i <= mid; i++) {
+            op[mid + i] = a[j++];
+            op[mid - i] = a[j++];
+        }
+        if (a.length % 2 == 0)
+            op[mid + i] = a[j];
 
+        System.out.println("Pendulum arrangement:");
+        for (i = 0; i < a.length; i++)
+            System.out.print(op[i] + " ");
+        System.out.println();
     }
 }
